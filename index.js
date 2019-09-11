40a511898de5a70ac7c52cc99459771703f18584
Jonas Horvei <jonashorvei1@gmail.com>
2019-09-11 05:35:38 +0200

    Added delete functionality for DB and added error logger.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-require('dotenv').config()
-
 const express = require('express')
 const app = express()
+require('dotenv').config()
 
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
@@ -20,30 +19,6 @@ app.get('/', (req, res) => {
 })
 
 
-let persons = [
-        {
-          name: "Ada Lovelace",
-          number: "39-44-5323523",
-          id: 2
-        },
-        {
-          name: "Dan Abramov",
-          number: "12-43-234345",
-          id: 3
-        },
-        {
-          name: "Mary Poppendieck",
-          number: "39-23-6423122",
-          id: 4
-        },
-          {
-          name: "Jonas Horvei",
-          number: "13132313",
-          id: 5
-        }
-      
-]
-
 const generateId = () => {
   const maxId = persons.length > 0 ? 
     Math.max(...persons.map(n => n.id))
@@ -56,23 +31,22 @@ app.get('/info', (req, res) => {
   res.send("Phonebook has info for " + persons.length + " people<br/><br />" + Date(Date.now().toString()))
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response,) => {
   Person.find({}).then (persons => {
     response.json(persons.map(person => person.toJSON()))
   })
 }) 
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person.toJSON())
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if(person) {
+        response.json(person.toJSON())
+      } else {
+        response.status(404).end()
+      }
   })
-  // const id = Number(request.params.id)
-  // const person = persons.find(person => person.id === id)
-  // if(person) {
-
-  // } else {
-  //   response.status(400).end()
-  // }
+  .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -110,12 +84,22 @@ app.post('/api/persons', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-   const id = Number(request.params.id)
-   persons = persons.filter(person => person.id !== id)
-
-   response.status(204).end
-
+   Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(erorr => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  if(error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001  
 app.listen(PORT, () => {
