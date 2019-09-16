0e8715eba730bca892942406b4163b159cfa9cbc
Jonas Horvei <jonashorvei1@gmail.com>
2019-09-16 16:45:09 +0200

    Validation if name or number is too short

--- index.js
+++ index.js
@@ -84,14 +84,6 @@ app.post('/api/persons', (request, response, next) => {
     })
   }
 
-  // const nameExists = persons.find(person => person.name === body.name)
-
-  // if(nameExists) {
-  //   return response.status(400).json({
-  //     error: 'Name must be unique'
-  //   })
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number,
@@ -119,7 +111,8 @@ const errorHandler = (error, request, response, next) => {
   if(error.name === 'CastError' && error.kind === 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if(error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    console.log(error.message)
+    return response.status(400).send({ error: error.message })
   }
 
   next(error)
